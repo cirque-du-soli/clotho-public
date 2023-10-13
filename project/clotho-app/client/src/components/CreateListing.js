33fa1729249993c2be2221576219a46659c5bebb
Dustin Ruck <dustin.ruck@gmail.com>
2023-10-13 11:45:42 -0400

    Put Some Strap on CreateListing

--- project/clotho-app/client/src/components/CreateListing.js
+++ project/clotho-app/client/src/components/CreateListing.js
@@ -1,36 +1,16 @@
+// CreateListing.js
 import React, { useState } from 'react';
-import axios from 'axios';
-
+import axios from 'axios'; 
 import {
-    Row,
-    Col,
     Card,
     CardHeader,
-    CardTitle,
     CardBody,
-    CardText,
-    CardFooter,
-    Table,
-    Button,
     Form,
     FormGroup,
     Input,
     Label,
-    Collapse,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    UncontrolledDropdown,
-    InputGroup,
-    NavbarBrand,
-    Navbar,
-    NavLink,
-    Nav,
-    Container,
-    Modal,
-    NavbarToggler,
-    ModalHeader
-    
+    Button,
+    Container
 } from "reactstrap";
 
 const CreateListing = () => {
@@ -49,9 +29,9 @@ const CreateListing = () => {
     setFormData(prevState => ({ ...prevState, [name]: value }));
   };
 
-//   const handleImageChange = (e) => {
-//     setFormData(prevState => ({ ...prevState, image: e.target.files[0] }));
-//   };
+  const handleImageChange = (e) => {
+    setFormData(prevState => ({ ...prevState, image: e.target.files[0] }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -59,39 +39,62 @@ const CreateListing = () => {
     for (const key in formData) {
       form.append(key, formData[key]);
     }
-    const response = await axios.post('api/listings', form);
+    const response = await axios.post('/listings', form);
     console.log(response.data);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="title" placeholder="Title" onChange={handleInputChange} required />
-        <textarea name="description" placeholder="Description" onChange={handleInputChange} required></textarea>
-        <input type="number" name="price" placeholder="Price" onChange={handleInputChange} required />
-        <select name="categoryId" onChange={handleInputChange}>
-          {/* TODO: Fetch and list categories here */}
-          <option value="1">Category 1</option>
-          <option value="2">Category 2</option>
-          {/* ... */}
-        </select>
-        <select name="genderId" onChange={handleInputChange}>
-          {/* TODO: Fetch and list genders here */}
-          <option value="1">Male</option>
-          <option value="2">Female</option>
-          {/* ... */}
-        </select>
-        <select name="sizeId" onChange={handleInputChange}>
-          {/* TODO: Fetch and list sizes here */}
-          <option value="1">Small</option>
-          <option value="2">Medium</option>
-          <option value="3">Large</option>
-          {/* ... */}
-        </select>
-        {/* <input type="file" name="image" onChange={handleImageChange} required /> */}
-        <button type="submit">Create Listing</button>
-      </form>
-    </div>
+    <Container>
+      <Card>
+        <CardHeader>Create New Listing</CardHeader>
+        <CardBody>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label for="title">Title</Label>
+              <Input type="text" name="title" id="title" placeholder="Title" onChange={handleInputChange} required />
+            </FormGroup>
+            <FormGroup>
+              <Label for="description">Description</Label>
+              <Input type="textarea" name="description" id="description" placeholder="Description" onChange={handleInputChange} required />
+            </FormGroup>
+            <FormGroup>
+              <Label for="price">Price</Label>
+              <Input type="number" name="price" id="price" placeholder="Price" onChange={handleInputChange} required />
+            </FormGroup>
+            <FormGroup>
+              <Label for="categoryId">Category</Label>
+              <Input type="select" name="categoryId" id="categoryId" onChange={handleInputChange}>
+                <option value="1">Category 1</option>
+                <option value="2">Category 2</option>
+                {/* ... */}
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="genderId">Gender</Label>
+              <Input type="select" name="genderId" id="genderId" onChange={handleInputChange}>
+                <option value="1">Male</option>
+                <option value="2">Female</option>
+                {/* ... */}
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="sizeId">Size</Label>
+              <Input type="select" name="sizeId" id="sizeId" onChange={handleInputChange}>
+                <option value="1">Small</option>
+                <option value="2">Medium</option>
+                <option value="3">Large</option>
+                {/* ... */}
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="image">Image</Label>
+              <Input type="file" name="image" id="image" onChange={handleImageChange} required />
+            </FormGroup>
+            <Button color="primary" type="submit">Create Listing</Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </Container>
   );
 };
 
