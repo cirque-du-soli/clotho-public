92a445ca27092028276c32fae13f5437b9af8395
Meredith <124798853+Mer-W@users.noreply.github.com>
2023-10-16 10:33:22 -0400

    fixed image api routes

--- project/clotho-app/server/routes/listing-images.js
+++ project/clotho-app/server/routes/listing-images.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const listingImageController = require('../controllers/listing-image-controller');
 
-router.post('/listingimages/upload', listingImageController.uploadPhoto);
-router.get('/listingimages/:id', listingImageController.getPhotos);
+router.post('/upload', listingImageController.uploadPhoto);
+router.get('/:id', listingImageController.getPhotos);
 
 module.exports = router;
