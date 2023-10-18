247e9d55e99a5ac54e0057395b13b195168b044f
Meredith <124798853+Mer-W@users.noreply.github.com>
2023-10-18 09:46:11 -0400

    profile css

--- project/clotho-app/client/src/components/Pages/UserProfilePrivate.js
+++ project/clotho-app/client/src/components/Pages/UserProfilePrivate.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import useAxiosJWT from '../../hooks/useAxiosJWT';
 import axios from '../../api/axios';
+import { Link } from 'react-router-dom';
 
 import { Button, Container, Row, Col, Card, CardBody, CardImg, CardFooter, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 
@@ -46,33 +47,44 @@ function UserProfilePrivate() {
               }
 
     return (
-        <Container>
-            <Row>
-                <Col md={4}>
-                    <img src={user.avatar} alt={`${user.username}'s avatar`} className="img-fluid rounded-circle" />
+        <div className='col-10 offset-1'>
+            <div className='row m-5'></div>
+            <div className='row my-5'>
+
+                <div className='col-2 col-md-1'>
+                    <img src={user.avatar} alt={`${user.username}'s avatar`} className="img-fluid rounded-circle" width={60} />
+                </div>
+                <div className='col-1 text-start'>
+
                     <h2>{user.username}</h2>
                     <p>{user.email}</p>
-                </Col>
-      
 
+                </div>
+            </div>
+            <div className='row m-5'></div>
 
                 <Row>
-                <h6>Selling</h6>
-      {listings.map(listing => (
-        <Col md="4" key={listing.id}>
-          <Card>
-            <CardImg top width="100%" src={listing.thumbnail} alt="listing image" />
-            <CardFooter>
-              <CardTitle tag="h5">
-              ${(listing.price / 100).toFixed(2)}
-              </CardTitle>
-            </CardFooter>
-          </Card>
-        </Col>
-      ))}
+                <h4>Selling</h4>
     </Row>
-            </Row>
-        </Container>
+    <Row>
+
+{listings.map(listing => (
+<Col md="2" className="my-2 p-1" key={listing.id}>
+    <Link to={`/products/${listing.id}`}>
+        <Card className='border-0 rounded-0'>
+            <img className='border-0 rounded-0' top width="100%" src={listing.thumbnail} alt="listing image" />
+        </Card>
+        <Row className='px-3 fs-5'>
+            ${listing.price}
+        </Row>
+    </Link>
+</Col>
+))}
+
+
+</Row>
+       
+        </div>
     );
 }
 
