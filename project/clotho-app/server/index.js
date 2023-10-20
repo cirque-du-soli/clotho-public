65226f1c63933970dcf2a29d26c0e1b10007998d
Dustin Ruck <dustin.ruck@gmail.com>
2023-10-20 13:37:02 -0400

    Revert "2nd try"
    This reverts commit c885e122e777e093c58dcd75b453f54479bf45b9.

--- project/clotho-app/server/index.js
+++ project/clotho-app/server/index.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const db = require('./models');
 const cors = require('cors');
 
 // import multer from 'multer';
 // import sharp;
 
-
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-  
-  const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 
 
