58399707eb6728f600f00e0b36e21a2135d28e88
Soliloquy Yarrow <soliloquy.yarrow@gmail.com>
2023-10-23 04:21:48 -0400

    Revert "Merge branch 'main' of https://github.com/Mer-W/clotho"
    This reverts commit 185bba3c5381e7d6077f22762bf7a3c0ec28e803, reversing
    changes made to b182fd6a2e370b25e8108241cca5643a0160fc02.

--- project/clotho-app/server/config/config.js
+++ project/clotho-app/server/config/config.js
@@ -1,51 +1,26 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// module.exports = {
+module.exports = {
 
-//   "development": {
-//     "username": process.env.DB_USER,
-//     "password": process.env.DB_PASSWORD,
-//     "database": process.env.DB_NAME,
-//     "host": process.env.DB_HOST,
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": process.env.DB_USER,
-//     "password": process.env.DB_PASSWORD,
-//     "database": process.env.DB_NAME,
-//     "host": process.env.DB_HOST,
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": process.env.DB_USER,
-//     "password": process.env.DB_PASSWORD,
-//     "database": process.env.DB_NAME,
-//     "host": process.env.DB_HOST,
-//     "dialect": "mysql"
-//   }
-// }
-
-require('dotenv').config({ path: "../.env" });
-module.exports ={
   "development": {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
